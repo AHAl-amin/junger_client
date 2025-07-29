91d99f4902f08869c270c86480ecb1e4c6cf8af9
Alamin Hossain <mdalaminhossain573190@gmail.com>
2025-07-29 18:29:47 +0600

    added language dropdown

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Bannerbg from '../../../public/image/banner.png'
+import VoiceImg from '../../../public/image/voiceIcon.png'
+import bannerMiddle from '../../../public/image/bannerMiddle.png'
+
+function Banner() {
+  return (
+   <div
+  className=" bg-cover bg-center"
+  style={{ backgroundImage: `url(${Bannerbg})` }}
+>
+  <div className=' relative flex flex-col items-center justify-center h-[calc(100vh-100px)]   text-white text-center'>
+    <h1 className='text-[#168F79] font-extrabold text-7xl product italic mt-10  '>Voice Survey</h1>
+    <p className='text-xl font-semibold text-[#637270] md:w-1/3 text-center mt-4'>Answer a few questions to help us improve our services. Your feedback matters to us!</p>
+    <div className='absolute mt-26 '>
+      <div className=' flex justify-center items-center '>
+        <img src={VoiceImg} alt="" className='w-80 h-80 ' />
+      </div>
+    </div>
+      <img src={bannerMiddle} className='w-full md:px-20 mt-10 ' alt="" />
+    <button className='px-14 cursor-pointer rounded-full py-3 mt-10 bg-gradient-to-t from-[#02362E] to-[#298F7D]' >Get Started</button>
+  </div>
+</div>
+  )
+}
+
+export default Banner
+
